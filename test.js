fe32dd44025555d58eb157c82c535432121dd2de
gyson <eilian.yunsong@gmail.com>
2015-10-30 13:55:43 -0400

    more test

--- test.js
+++ test.js
@@ -2,12 +2,13 @@
 
 'use strict'
 
-// const co = require('co')
+const co = require('co')
 const assert = require('assert')
 const convert = require('./index')
+const compose = require('koa-compose')
 
-describe('Koa Convert', function () {
-  it('should works', function (done) {
+describe('Koa Convert', () => {
+  it('should works', () => {
     let call = []
     let ctx = {}
     let mw = convert(function * (next) {
@@ -15,15 +16,14 @@ describe('Koa Convert', function () {
       call.push(1)
     })
 
-    mw(ctx, function () {
-      done(new Error('this should not be called'))
+    return mw(ctx, function () {
+      call.push(2)
     }).then(function () {
       assert.deepEqual(call, [1])
-      done()
     })
   })
 
-  it('should works with `yield next`', function (done) {
+  it('should works with `yield next`', () => {
     let call = []
     let ctx = {}
     let mw = convert(function * (next) {
@@ -33,16 +33,15 @@ describe('Koa Convert', function () {
       call.push(3)
     })
 
-    mw(ctx, function () {
+    return mw(ctx, function () {
       call.push(2)
       return Promise.resolve()
     }).then(function () {
       assert.deepEqual(call, [1, 2, 3])
-      done()
     })
   })
 
-  it('should works with `yield* next`', function (done) {
+  it('should works with `yield* next`', () => {
     let call = []
     let ctx = {}
     let mw = convert(function * (next) {
@@ -52,12 +51,58 @@ describe('Koa Convert', function () {
       call.push(3)
     })
 
-    mw(ctx, function () {
+    return mw(ctx, function () {
       call.push(2)
       return Promise.resolve()
     }).then(function () {
       assert.deepEqual(call, [1, 2, 3])
-      done()
+    })
+  })
+
+  it('should works with koa-compose', () => {
+    let call = []
+    let context = {}
+    let _context
+    let mw = compose([
+      function * name (next) {
+        call.push(1)
+        yield next
+        call.push(11)
+      },
+      (ctx, next) => {
+        call.push(2)
+        return next().then(() => {
+          call.push(10)
+        })
+      },
+      function * (next) {
+        call.push(3)
+        yield* next
+        call.push(9)
+      },
+      co.wrap(function * (ctx, next) {
+        call.push(4)
+        yield next()
+        call.push(8)
+      }),
+      function * (next) {
+        try {
+          call.push(5)
+          yield next
+        } catch (e) {
+          call.push(7)
+        }
+      },
+      (ctx, next) => {
+        _context = ctx
+        call.push(6)
+        throw new Error()
+      }
+    ].map(convert))
+
+    return mw(context).then(() => {
+      assert.equal(context, _context)
+      assert.deepEqual(call, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
     })
   })
 })
