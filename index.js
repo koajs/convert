e6628be831eeb2f0f8bf887c7eb4c14fe3ac8094
gyson <eilian.yunsong@gmail.com>
2015-10-30 13:29:29 -0400

    use standard

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-'use strict';
+'use strict'
 
 const co = require('co')
 
 module.exports = convert
 
-function convert(mw) {
-    if (typeof mw !== 'function') {
-        throw new TypeError(mw + ' is not function')
+function convert (mw) {
+  if (typeof mw !== 'function') {
+    throw new TypeError(mw + ' is not function')
+  }
+  if (mw.constructor.name === 'GeneratorFunction') {
+    return function (ctx, next) {
+      return co.call(ctx, mw.call(ctx, createGenerator(next)))
     }
-    if (mw.constructor.name === 'GeneratorFunction') {
-        return function (ctx, next) {
-            return co.call(ctx, mw.call(ctx, createGenerator(next)))
-        }
-    } else {
-        // assume it's Promise-based middleware
-        return mw
-    }
+  } else {
+    // assume it's Promise-based middleware
+    return mw
+  }
 }
 
-function* createGenerator(next) {
-    return yield next()
+function * createGenerator (next) {
+  return yield next()
 }
--- test.js
+++ test.js
@@ -1,61 +1,63 @@
-'use strict';
+/* global describe, it */
 
-const co = require('co')
+'use strict'
+
+// const co = require('co')
 const assert = require('assert')
 const convert = require('./index')
 
 describe('Koa Convert', function () {
-    it('should works', function (done) {
-        let call = []
-        let ctx = {}
-        let mw = convert(function* (next) {
-            assert.ok(ctx === this)
-            call.push(1)
-        })
-
-        mw(ctx, function () {
-            done(new Error('this should not be called'))
-        }).then(function () {
-            assert.deepEqual(call, [1])
-            done()
-        })
+  it('should works', function (done) {
+    let call = []
+    let ctx = {}
+    let mw = convert(function * (next) {
+      assert.ok(ctx === this)
+      call.push(1)
     })
 
-    it('should works with `yield next`', function (done) {
-        let call = []
-        let ctx = {}
-        let mw = convert(function* (next) {
-            assert.ok(ctx === this)
-            call.push(1)
-            yield next
-            call.push(3)
-        })
+    mw(ctx, function () {
+      done(new Error('this should not be called'))
+    }).then(function () {
+      assert.deepEqual(call, [1])
+      done()
+    })
+  })
 
-        mw(ctx, function () {
-            call.push(2)
-            return Promise.resolve()
-        }).then(function () {
-            assert.deepEqual(call, [1, 2, 3])
-            done()
-        })
+  it('should works with `yield next`', function (done) {
+    let call = []
+    let ctx = {}
+    let mw = convert(function * (next) {
+      assert.ok(ctx === this)
+      call.push(1)
+      yield next
+      call.push(3)
     })
 
-    it('should works with `yield* next`', function (done) {
-        let call = []
-        let ctx = {}
-        let mw = convert(function* (next) {
-            assert.ok(ctx === this)
-            call.push(1)
-            yield* next
-            call.push(3)
-        })
-
-        mw(ctx, function () {
-            call.push(2)
-            return Promise.resolve()
-        }).then(function () {
-            assert.deepEqual(call, [1, 2, 3])
-            done()
-        })
+    mw(ctx, function () {
+      call.push(2)
+      return Promise.resolve()
+    }).then(function () {
+      assert.deepEqual(call, [1, 2, 3])
+      done()
     })
+  })
+
+  it('should works with `yield* next`', function (done) {
+    let call = []
+    let ctx = {}
+    let mw = convert(function * (next) {
+      assert.ok(ctx === this)
+      call.push(1)
+      yield* next
+      call.push(3)
+    })
+
+    mw(ctx, function () {
+      call.push(2)
+      return Promise.resolve()
+    }).then(function () {
+      assert.deepEqual(call, [1, 2, 3])
+      done()
+    })
+  })
 })
